992f1f5fd5c52ff224049c2e1d18c77b54d02c46
Denis_L <denis88lyapin@gmail.com>
2023-07-18 22:54:28 +0300

    added code to head_hunter_api.py

---
+++ src/head_hunter_api.py
@@ -0,0 +1,77 @@
+import requests
+from pprint import pprint
+# [78638, 1740, 3529, 80660, 1122462, 1092837, 105904, 9501038, 1178447, 1455]
+class HeadHunterAPI:
+    """Класс для получения данных о работодателях и их вакансиях с сайта hh.ru."""
+
+    def __init__(self) -> None:
+        self.employers_ids: list = [1178447, 1455]
+        self.url_employers = 'https://api.hh.ru/employers/'
+        self.url_vacancies = 'https://api.hh.ru/vacancies'
+        self.headers = {'User-Agent': '150014979'}
+
+    def get_data_company(self) -> list[dict]:
+        '''Получить данные о работодателях'''
+        data = []
+        for employer_id in self.employers_ids:
+            try:
+                response = requests.get(f"{self.url_employers}{employer_id}", headers=self.headers)
+                if response.status_code == 200:
+                    data_tmp = response.json()
+                    data_company = {
+                                    'employer_id': data_tmp['id'],
+                                    'name': data_tmp['name'],
+                                    'area': data_tmp['area']['name'],
+                                    'open_vacancies': data_tmp['open_vacancies'],
+                                    'url': data_tmp['site_url']
+                                    }
+                    data.append(data_company)
+                    print(f"Данные о компани '{data_tmp['name']}' получены.")
+                else:
+                    print(f'Ошибка при получении данных о компаниях. Код ошибки: {response.status_code}')
+                    break
+            except requests.exceptions.RequestException as e:
+                print(f"{e}")
+        return data
+
+    def get_data_vacancies(self) -> list[dict]:
+        '''Получить данные о вакансиях работодателей'''
+        data = []
+        for employer_id in self.employers_ids:
+            try:
+                page = 0
+                pages = 1
+                while page < pages:
+                    params = {
+                        'employer_id': employer_id,
+                        'per_page': 100,
+                        'page': page,
+                        'archived': False
+                    }
+                    response = requests.get(url=self.url_vacancies, params=params, headers=self.headers)
+                    if response.status_code == 200:
+                        data_tmp = response.json()
+                        data.extend(data_tmp['items'])
+                        print(f"Данные о {data_tmp['items'][0]['employer']['name']} страница {page} получены.")
+                        page += 1
+                        pages = data_tmp['pages']
+                    else:
+                        print(f'Ошибка при получении вакансий. Код ошибки: {response.status_code}')
+                        break
+            except requests.exceptions.RequestException as e:
+                print(f"{e}")
+        return data
+
+
+if __name__ == '__main__':
+    a = HeadHunterAPI()
+    c = a.get_data_vacancies()
+    pprint(c)
+    # params = {
+    #     'employer_id': 78638,
+    #     'per_page': 100,
+    #     'page': 0}
+    # headers = {'User-Agent': '150014979'}
+    # response = requests.get(url='https://api.hh.ru/vacancies', params=params, headers=headers)
+    # d = response.json()
+    # pprint(d)
